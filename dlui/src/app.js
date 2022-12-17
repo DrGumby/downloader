4b7bb12a1f2262f83eb06c86e8277aad65ac4e32
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2022-12-17 12:40:20 +0100

    dlui: implement periodic checking and downloading

--- dlui/src/app.js
+++ dlui/src/app.js
@@ -1,3 +1,5 @@
+import { parse } from 'content-disposition-attachment';
+
 const convert_button = document.querySelector(".convert-button");
 const url_input = document.querySelector(".url-input");
 const progress_bar = document.querySelector("#progress-bar");
@@ -48,6 +50,91 @@ function download_start(url) {
         .then((res) => {
             reset_error();
             init_progress();
+            return res.json();
+        })
+        .then((data) => {
+            const current_download_id = data.id;
+            setTimeout(() => download_update(current_download_id), 1000);
+        })
+        .catch((err) => {
+            reset_progress();
+            show_error(err);
+        });
+}
+
+function download_delete(id) {
+    fetch(`http://localhost:8000/download/${id}`, {
+        method: "DELETE",
+    });
+}
+
+function download_transfer(id) {
+    fetch(`http://localhost:8000/download/${id}`, {
+        method: "GET",
+    })
+        .then((res) => {
+            const url = window.URL.createObjectURL(new Blob([res.data]));
+            const link = document.createElement("a");
+            const filename = parse(res.headers.get("content-disposition"));
+            link.href = url;
+            console.log(filename.filename);
+            link.setAttribute("download", filename.filename);
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            download_delete(id);
+        })
+        .catch((err) => {
+            reset_progress();
+            show_error(err);
+        });
+}
+
+function handle_update(id, download_record) {
+    switch (download_record.status) {
+        case "ERROR":
+            reset_progress();
+            hide_postprocessing();
+            show_error("Unidentified error returned by 'yt-dlp'.");
+            break;
+
+        case "IN_PROGRESS":
+            hide_postprocessing();
+            progress_bar.value = download_record.progress;
+            setTimeout(() => download_update(id), 1000);
+            break;
+
+        case "POSTPROCESSING":
+            show_postprocessing();
+            progress_bar.value = download_record.progress;
+            setTimeout(() => download_update(id), 1000);
+            break;
+
+        case "FINISHED":
+            console.log("Download finished on backend. Transferring!");
+            progress_bar.value = download_record.progress;
+            hide_postprocessing();
+            download_transfer(id);
+            break;
+
+        default:
+            reset_progress()
+            hide_postprocessing();
+            show_error(`Invalid status ${download_record.status}`);
+            break;
+    }
+}
+
+function download_update(id) {
+    fetch(`http://localhost:8000/download/${id}/status`, {
+        method: "GET",
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            handle_update(id, data);
         })
         .catch((err) => {
             reset_progress();
