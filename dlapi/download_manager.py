4b940d8c2eb397b7c38b74f6504620f8a79397cf
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2022-12-17 09:11:18 +0100

    dlapi: initial commit

---
+++ dlapi/download_manager.py
@@ -0,0 +1,124 @@
+import asyncio
+import logging
+
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import Union
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class DLStatus(str, Enum):
+    """Download status enumeration"""
+
+    IN_PROGRESS = "IN_PROGRESS"
+    FINISHED = "FINISHED"
+    ERROR = "ERROR"
+
+
+@dataclass
+class Download:
+    """Single download entry"""
+
+    id: int
+    url: str
+    status: DLStatus
+    progress: int
+    path: Union[Path, None]
+
+
+class DownloadManager:
+    """Main download manager. Used to store the download information."""
+
+    def __init__(self) -> None:
+        self.access_lock = asyncio.Lock()
+        self.current_id = 0
+        self.downloads = dict()
+
+    async def insert_download(self, url: str) -> int:
+        """Initialize a single download from given URL.
+
+        Args:
+            url (str): URL string from which to download
+
+        Returns:
+            int: ID of inserted entry
+        """
+        async with self.access_lock:
+            id = self.current_id
+
+            # Should never happen, but check anyways
+            assert id not in self.downloads
+
+            dl = Download(
+                id=id,
+                url=url,
+                status=DLStatus.IN_PROGRESS,
+                progress=0,
+                path=None,
+            )
+
+            self.downloads[id] = dl
+            self.current_id += 1
+
+        logger.debug(f"Inserted new download with id {id}")
+        return id
+
+    async def get_download(self, id: int) -> Download:
+        """Get the download info for given id.
+
+        Args:
+            id (int): ID of download info to return.
+
+        Returns:
+            Download: Returned download info object.
+        """
+        async with self.access_lock:
+            dl = self.downloads[id]
+
+        return dl
+
+    async def update_download(
+        self,
+        id: int,
+        status: Union[DLStatus, None] = None,
+        progress: Union[int, None] = None,
+        path: Union[Path, None] = None,
+    ):
+        """Update a specified download record.
+
+        Args:
+            id (int): ID of record to be updated.
+            status (Union[DLStatus, None]): Status to be set (or None for no change).
+            progress (Union[int, None]): Progress to be set (or None for no change).
+            path (Union[Path, None]): Path to be set (or None for no change).
+        """
+        async with self.access_lock:
+            dl: Download = self.downloads[id]
+
+            if status is not None:
+                dl.status = status
+            if progress is not None:
+                dl.progress = progress
+            if path is not None:
+                dl.path = path
+
+            # Not sure if this is necessary
+            self.downloads[dl.id] = dl
+
+        logger.debug(
+            f"Updated download id: {id} with status: {status}, progress: {progress}, path: {path}"
+        )
+
+    async def delete_download(self, id: int):
+        """Remove given record from the download dict.
+
+        Args:
+            id (int): ID to be removed.
+        """
+        async with self.access_lock:
+            del self.downloads[id]
+
+        logger.debug(f"Removed download with id: {id}")
---
+++ dlapi/main.py
@@ -0,0 +1,136 @@
+import asyncio
+import logging
+import yt_dlp
+
+from pathlib import Path
+
+from fastapi import BackgroundTasks, FastAPI, HTTPException, status
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from download_manager import *
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+async def yt_dlp_hooks(id: int, ytdl_info: dict()):
+    """Main hook function handling progress hooks returned by
+    yt-dlp module.
+
+    Args:
+        id (int): ID of the download record inside DownloadManager.
+        ytdl_info (dict): Info returned by yt-dlp.
+    """
+
+    async def finished(id: int, ytdl_info: dict()):
+        filename = ytdl_info["filename"]
+        await manager.update_download(
+            id, status=DLStatus.FINISHED, progress=100, path=Path(filename)
+        )
+        logger.debug(f"Downloaded to file: {filename}")
+
+    async def downloading(id: int, ytdl_info: dict()):
+        try:
+            # These fields may be missing, ignore errors
+            dl_bytes = ytdl_info["downloaded_bytes"]
+            total_bytes = ytdl_info["total_bytes"]
+            percentage = int((dl_bytes / total_bytes) * 100)
+
+            await manager.update_download(id, progress=percentage)
+
+            logger.debug(
+                f"Downloaded {dl_bytes} out of {total_bytes}, progress: {percentage}"
+            )
+        except KeyError:
+            logger.warning(f"Could not get status update info")
+
+    async def error(id: int, ytdl_info: dict()):
+        await manager.update_download(id, status=DLStatus.ERROR)
+        logger.error(f"Failed to download from id: {id}")
+
+    if ytdl_info["status"] == "finished":
+        await finished(id, ytdl_info)
+    elif ytdl_info["status"] == "downloading":
+        await downloading(id, ytdl_info)
+    elif ytdl_info["status"] == "error":
+        await error(id, ytdl_info)
+    else:
+        assert False
+
+
+def start_download(download_id: int, url: str):
+    """Background function run whenever a download should start.
+
+    Args:
+        download_id (int): Assigned download ID used by DownloadManager.
+        url (str): URL to download from.
+    """
+    ydl_opts = {"progress_hooks": [lambda d: asyncio.run(yt_dlp_hooks(download_id, d))]}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(url)
+
+
+origins = [
+    "*"
+]
+
+app = FastAPI()
+manager = DownloadManager()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.post("/download/", status_code=status.HTTP_202_ACCEPTED)
+async def download_start(start: str, background_tasks: BackgroundTasks):
+    try:
+        with yt_dlp.YoutubeDL({}) as ydl:
+            # Just try extracting info before downloading
+            ydl.extract_info(start, download=False)
+    except yt_dlp.utils.DownloadError:
+        raise HTTPException(status_code=400, detail="Invalid Youtube link")
+
+    download_id = await manager.insert_download(start)
+    background_tasks.add_task(start_download, download_id, start)
+    return dict(id=download_id)
+
+
+@app.get("/download/{id}/status")
+async def download_status(id: int):
+    try:
+        dl = await manager.get_download(id)
+        return dict(
+            id=dl.id,
+            status=dl.status,
+            progress=dl.progress,
+        )
+    except KeyError:
+        raise HTTPException(status_code=404, detail="ID not found")
+
+
+@app.get("/download/{id}")
+async def download_exec(id: int):
+    try:
+        dl = await manager.get_download(id)
+        if dl.status != DLStatus.FINISHED or dl.path is None:
+            raise HTTPException(status_code=404, detail="File not ready")
+
+        return FileResponse(dl.path)
+    except KeyError:
+        raise HTTPException(status_code=404, detail="ID not found")
+
+
+@app.delete("/download/{id}")
+async def download_delete(id: int):
+    try:
+        await manager.delete_download(id)
+        return "delete"
+    except KeyError:
+        raise HTTPException(status_code=404, detail="ID not found")
