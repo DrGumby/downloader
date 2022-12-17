86daaa76031255c4e4063c8880fa1b568faffa92
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2022-12-17 12:42:23 +0100

    dlapi: expose content-disposition over cors

--- dlapi/main.py
+++ dlapi/main.py
@@ -128,7 +128,8 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=False,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
+    expose_headers=["content-disposition"],
 )
 
 
